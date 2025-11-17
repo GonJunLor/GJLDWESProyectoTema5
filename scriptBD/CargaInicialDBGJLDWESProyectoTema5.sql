c59878adb3abeb88aeb7e8cdac231b7f4eaf02f8
GonJunLor <gonzalo.junlor@educa.jcyl.es>
2025-11-17 13:00:09 +0100

    fix: Hecho ej2

--- scriptBD/CargaInicialDBGJLDWESProyectoTema5.sql
+++ scriptBD/CargaInicialDBGJLDWESProyectoTema5.sql
@@ -11,8 +11,25 @@ insert into T02_Departamento values
         ('AUT','Departamento de automocion.',now(),5235.8,null),
         ('ELE','Departamento de electricidad.',now(),2275.1,null),
         ('MAT','Departamento de matematicas.',now(),735.2,null),
-        ('ING','Departamento de ingles.',now(),235.9,now()
-);
+        ('ING','Departamento de ingles.',now(),235.9,now())
+;
 
-insert into Usuario values ('admin','d6ed7eb369f21acd3d3d66a96de946cc2b514e4279827bf8a7ca9d7005514b27');
+
+INSERT INTO T01_Usuario (T01_CodUsuario,T01_Password,T01_DescUsuario)
+                VALUES
+        ('vero',SHA2('veropaso',256),'Véro Grué'),
+        ('heraclio',SHA2('heracliopaso',256),'Heraclio Borbujo'),
+        ('alvaroA',SHA2('alvaroApaso',256),'Alvaro Allen'),
+        ('alejandro',SHA2('alejandropaso',256),'Alejandro De La Huerga'),
+        ('alvaroG',SHA2('alvaroGpaso',256),'Alvaro García'),
+        ('gonzalo',SHA2('gonzalopaso',256),'Gonzalo Junquera'),
+        ('cristian',SHA2('cristianpaso',256),'Cristian Mateos'),
+        ('alberto',SHA2('albertopaso',256),'Alberto Méndez'),
+        ('enrique',SHA2('enriquepaso',256),'Enrique Nieto'),
+        ('james',SHA2('jamespaso',256),'James Edward Nuñez'),
+        ('oscar',SHA2('oscarpaso',256),'Oscar Pozuelo'),
+        ('jesus',SHA2('jesuspaso',256),'Enrique Nieto'),
+        ('amor',SHA2('amorpaso',256),'Amor Rodriguez'),
+        ('albertoB',SHA2('albertoBpaso',256),'Alberto Bahillo')
+;
 
--- scriptBD/CreaDBGJLDWESProyectoTema5.sql
+++ scriptBD/CreaDBGJLDWESProyectoTema5.sql
@@ -13,9 +13,14 @@ create table if not exists DBGJLDWESProyectoTema5.T02_Departamento(
     T02_FechaBajaDepartamento datetime null
 )engine=innodb;
 
-create table if not exists DBGJLDWESProyectoTema5.Usuario(
-    nombre varchar(100) not null primary key,
-    contrasena varchar(64) not null
+create table if not exists DBGJLDWESProyectoTema5.T01_Usuario(
+    T01_CodUsuario varchar(10) not null primary key,
+    T01_Password varchar(64) not null,
+    T01_DescUsuario varchar(255) not null,
+    T01_NumConexiones int not null default 0,
+    T01_FechaHoraUltimaConexion datetime not null default now(),
+    T01_Perfil varchar (100) not null default 'usuario',
+    T01_ImagenUsuario BLOB default null
 )engine=innodb;
 
 create user if not exists 'userGJLDWESProyectoTema5'@'%' identified by '5813Libro-Puro';
